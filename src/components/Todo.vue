6b90a34635390b6b747833abdac659f6738c54af
Aya Matsubara <mats0035@algonquinlive.com>
2019-06-21 12:15:49 -0400

    Added a form and slots in the form component

---
+++ src/components/Todo.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <li v-for="(todo, index) in todos" :key="index" @click="todoDone(index)">{{ todo }}</li>
+  </ul>
+</template>
+<script>
+export default {
+  // props: ['todos']
+  props: {
+    todos: {
+      type: Array, required: true
+    }
+  },
+  methods: {
+    todoDone(index) {
+      console.log(index);
+      // $emit emits an event from the child component to the parent component $emitは、parentに情報を渡すときに使う
+      this.$emit("removeTodo", index);
+    }
+  }
+}
+</script>
